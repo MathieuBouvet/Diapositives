8307d304eb042fab671868c640ea13e2e8217bc3
MathieuBouvet <mathieubouvet42@gmail.com>
2018-09-28 22:59:56 +0200

    Prepare the PseudoSate skeleton

--- src/Diapo.js
+++ src/Diapo.js
@@ -1,3 +1,6 @@
+import {StateAttribute} from "./StateAttribute";
+import {PseudoState} from "./PseudoState";
+
 export class Diapo {
 	constructor(containerSelector, contentSelector, slidesSelector){
 		this.containerSelector = containerSelector;
@@ -5,9 +8,26 @@ export class Diapo {
 		this.slidesSelector = slidesSelector;
 
 		this.slides = jQuery(slidesSelector);
+
+		this.state = new PseudoState(this, {
+			currentSlide: 0,
+		})
+
+		jQuery(contentSelector).click(this.slideInc);
 	}
 
 	displaySlides(){
 		console.log(this.slides);
+	}
+
+	slideInc = () => {
+		this.state.set({
+			currentSlide: 2,
+		});
+		console.log("test async");
+	}
+
+	stateChanged(attrName, newValue){
+		console.log("Changed: "+attrName+" new Vallue: "+newValue);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import {Diapo} from "./Diapo";
+import {PseudoState} from "./PseudoState";
 
 (function($){
 	$(function(){
 		var diapo = new Diapo("#main", "article", ".diapo-slide");
 		diapo.displaySlides();
+		diapo.state.displayMe();
+
 	});
 	
 })(jQuery);
