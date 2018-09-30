3c7f2845336ac87359735f04d5e621e4e90404ee
MathieuBouvet <mathieubouvet42@gmail.com>
2018-09-30 20:34:48 +0200

    Edit scrollObserver to get the relevant scrollPosition

--- src/workers/ScrollObserver.js
+++ src/workers/ScrollObserver.js
@@ -2,10 +2,24 @@ import {DataWorker} from "./DataWorker";
 export class ScrollObserver extends DataWorker {
 
 	constructor(state){
-		super(state, ["currentSlide"]);
+		super(state, [
+			"mode",
+			"headerHeight",
+			"menuHeight",
+		]);
+
+		window.addEventListener('scroll', this.update.bind(this));
 	}
 
 	update(){
-		console.log(this);
+		var newScrollPosition;
+		if(this.state.get("mode") === "vertical"){
+			newScrollPosition = window.scrollY - this.state.get("headerHeight") + this.state.get("menuHeight");
+		}else{
+
+		}
+		this.state.set({
+			scrollPosition: newScrollPosition
+		});
 	}
 }
