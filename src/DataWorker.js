144403406c4c919b89f64e8fabe9c1eec31d5756
MathieuBouvet <mathieubouvet42@gmail.com>
2018-09-30 19:10:46 +0200

    Create Dataworker class, a parent class to register a change in some state properties

---
+++ src/DataWorker.js
@@ -0,0 +1,15 @@
+
+export class DataWorker {
+
+	constructor(state, dataToRegister){
+		this.state = state;
+		for(let i=0 ; i<dataToRegister.length ; i++){
+			this.state.registerObserver(dataToRegister[i], this.update.bind(this));
+		}
+	}
+
+	update(){
+		console.log(this);
+		console.log("Must be overridden in a child class");
+	}
+}
