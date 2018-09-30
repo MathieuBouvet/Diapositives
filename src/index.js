6e19fdcda1abb73148923c209419d0f34b561497
MathieuBouvet <mathieubouvet42@gmail.com>
2018-09-30 23:49:35 +0200

    Delay diapo initialisation to let css calc the slides height before grabing it in diapo state

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import {PseudoState} from "./PseudoState";
 (function($){
 	$(function(){
 		var arr = [1,2];
-		const [ test1, test2 ] = arr;
-		console.log(test1);
-		var diapo = new Diapo("#main", "article", ".diapo-slide");
-		diapo.displaySlides();
-		diapo.state.displayMe();
-
+		setTimeout(function(){
+			var diapo = new Diapo();
+		});
 	});
 	
 })(jQuery);
